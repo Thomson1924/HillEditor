b17ee792776366ad47f1d9cfa948c9cea84eb8e5
Tomasz Bączek <tb054379@student.ath.edu.pl>
2021-12-03 16:53:25 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml;
+
+namespace HillEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument newhill = new XmlDocument();
+            newhill.Load("Testhill-HS140-POL.xml");
+            var a = newhill.SelectNodes("inrun");
+
+            BasicHillEditor(newhill);
+            newhill.PreserveWhitespace = true;
+            newhill.Save("Testhill-HS140-POL.xml");
+            string finalDoc = newhill.OuterXml;
+        
+        }
+        static void BasicHillEditor(XmlDocument hill)
+        {
+            XmlElement root = hill.DocumentElement;
+            root.SetAttribute("version","DSJ4-1.8.0");
+        }
+    }
+}
